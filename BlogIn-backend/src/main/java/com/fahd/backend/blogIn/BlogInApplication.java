96b3c5a5c128354f4163b4b21b05f9b450d4c0a9
Fahd <fahds2005@icloud.com>
2025-02-11 23:40:41 -0800

    Backend module for 'BlogIn'

---
+++ BlogIn-backend/src/main/java/com/fahd/backend/blogIn/BlogInApplication.java
@@ -0,0 +1,13 @@
+package com.fahd.backend.blogIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogInApplication.class, args);
+	}
+
+}
---
+++ BlogIn-backend/src/test/java/com/fahd/backend/blogIn/BlogInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fahd.backend.blogIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
